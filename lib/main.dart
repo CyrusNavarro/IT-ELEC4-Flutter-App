02b0bae69a9774a9c002c28fe9e544204eab56a8
Cyrus Lance Navarro <cyruslance12@gmail.com>
2025-09-06 17:57:17 +0800

    Create home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+
+
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(App());
 }
@@ -6,11 +10,8 @@ void main() {
 class App extends StatelessWidget{
 @override
 Widget build (BuildContext context){
-  Scaffold homepage = Scaffold
-  (appBar: AppBar(title: Text('Homepage'))
-  );
    return MaterialApp(
-    home: homepage,
+    home: HomeScreen(),
     theme: ThemeData(
       appBarTheme: AppBarTheme(
         backgroundColor: Colors.red,
@@ -18,5 +19,5 @@ Widget build (BuildContext context){
       )
     ),
    );
-}
+  }
 }
